6efe7f8b3a03344340cacd82ba4be787bd45cba7
sefornes <41449429+sefornes@users.noreply.github.com>
2024-02-14 11:49:59 +0100

    feat: add allback_public_client_enabled, identifier_uris and login_url parameters

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 data "azuread_client_config" "current" {}
 
 resource "azuread_application" "this" {
-  display_name                 = var.display_name
-  service_management_reference = var.service_management_reference
-  owners                       = var.owners
-  sign_in_audience             = "AzureADMyOrg"
+  display_name                   = var.display_name
+  service_management_reference   = var.service_management_reference
+  owners                         = var.owners
+  device_only_auth_enabled       = var.device_only_auth_enabled
+  fallback_public_client_enabled = var.fallback_public_client_enabled
+  identifier_uris                = var.identifier_uris
+  sign_in_audience               = "AzureADMyOrg"
 
   lifecycle {
     precondition {
@@ -17,4 +20,5 @@ resource "azuread_application" "this" {
 resource "azuread_service_principal" "this" {
   client_id = azuread_application.this.client_id
   owners    = var.owners
+  login_url = var.login_url
 }
--- variables.tf
+++ variables.tf
@@ -3,6 +3,24 @@ variable "display_name" {
   type        = string
 }
 
+variable "device_only_auth_enabled" {
+  description = "(Optional) Specifies whether this application supports device authentication without a user."
+  type        = bool
+  default     = false
+}
+
+variable "fallback_public_client_enabled" {
+  description = " (Optional) Specifies whether the application is a public client. Appropriate for apps using token grant flows that don't use a redirect URI."
+  type        = bool
+  default     = false
+}
+
+variable "identifier_uris" {
+  description = "(Optional) A set of user-defined URI(s) that uniquely identify an application within its Azure AD tenant, or within a verified custom domain if the application is multi-tenant."
+  type        = list(string)
+  default     = []
+}
+
 variable "service_management_reference" {
   description = "Reference application context information from a service management datbase, e.g. ServiceNow."
   type        = string
@@ -16,4 +34,10 @@ variable "owners" {
     condition     = length(var.owners) >= 2
     error_message = "At least two owners must be set."
   }
+}
+
+variable "login_url" {
+  description = "(Optional) The URL where the service provider redirects the user to Azure AD to authenticate. Azure AD uses the URL to launch the application from Microsoft 365 or the Azure AD My Apps. When blank, Azure AD performs IdP-initiated sign-on for applications configured with SAML-based single sign-on."
+  type        = string
+  default     = null
 }
