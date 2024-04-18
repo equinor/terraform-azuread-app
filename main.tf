18bfc13e881be7f7d3d8c858655330a6d54e353c
sondresjolyst <sonyslyst@gmail.com>
2024-04-18 14:28:04 +0200

    feat: add new variables and arguments
    This commit introduces multiple new variables and arguments such as homepage_url, logout_url, web_redirect_uris, public_redirect_uris, access_token_issuance, id_token_issuance, required_resource_access, and oauth2_permission_scopes

--- main.tf
+++ main.tf
@@ -6,15 +6,79 @@ resource "azuread_application" "this" {
   owners                         = var.owners
   device_only_auth_enabled       = var.device_only_auth_enabled
   fallback_public_client_enabled = var.fallback_public_client_enabled
-  identifier_uris                = var.identifier_uris
   sign_in_audience               = "AzureADMyOrg"
+
+  public_client {
+    redirect_uris = var.public_redirect_uris
+  }
+
+  web {
+    homepage_url  = var.homepage_url
+    logout_url    = var.login_url
+    redirect_uris = var.web_redirect_uris
+
+    implicit_grant {
+      access_token_issuance_enabled = var.access_token_issuance
+      id_token_issuance_enabled     = var.id_token_issuance
+    }
+  }
+
+  dynamic "required_resource_access" {
+    for_each = var.required_resource_access
+    content {
+      resource_app_id = required_resource_access.value.resource_app_id
+
+      resource_access {
+        id   = required_resource_access.value.resource_access.id
+        type = required_resource_access.value.resource_access.type
+      }
+    }
+  }
 
   lifecycle {
     precondition {
       condition     = contains(var.owners, data.azuread_client_config.current.object_id)
       error_message = "Current client (object ID: \"${data.azuread_client_config.current.object_id}\") must be set as owner."
     }
+    ignore_changes = [
+      identifier_uris,
+    ]
   }
+}
+
+resource "random_uuid" "scope_ids" {
+  count = length(var.oauth2_permission_scopes)
+}
+
+resource "azuread_application_permission_scope" "this" {
+  for_each = var.identifier_uris
+
+  admin_consent_description  = each.value.admin_consent_description
+  admin_consent_display_name = each.value.admin_consent_display_name
+
+  application_id = azuread_application.this.id
+  id             = each.value.id
+  scope_id       = random_uuid.scope_ids.result
+  type           = each.value.type
+
+  user_consent_description  = each.value.user_consent_description
+  user_consent_display_name = each.value.user_consent_display_name
+
+  value          = each.value.value
+}
+
+resource "azuread_application_identifier_uri" "default" {
+  depends_on = [azuread_application.this]
+
+  application_id = azuread_application.this.id
+  identifier_uri = "api://${azuread_application.this.client_id}"
+}
+
+resource "azuread_application_identifier_uri" "extra" {
+  for_each = var.identifier_uris
+
+  application_id = azuread_application.this.id
+  identifier_uri = each.value
 }
 
 resource "azuread_service_principal" "this" {
--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "fallback_public_client_enabled" {
 
 variable "identifier_uris" {
   description = "(Optional) A set of user-defined URI(s) that uniquely identify an application within its Azure AD tenant, or within a verified custom domain if the application is multi-tenant."
-  type        = list(string)
+  type        = set(string)
   default     = []
 }
 
@@ -40,4 +40,91 @@ variable "login_url" {
   description = "(Optional) The URL where the service provider redirects the user to Azure AD to authenticate. Azure AD uses the URL to launch the application from Microsoft 365 or the Azure AD My Apps. When blank, Azure AD performs IdP-initiated sign-on for applications configured with SAML-based single sign-on."
   type        = string
   default     = null
+}
+
+variable "homepage_url" {
+  description = "(Optional) Application home page."
+  type        = string
+  default     = null
+}
+
+variable "logout_url" {
+  description = "(Optional) The URL that will be used to sign out."
+  type        = string
+  default     = null
+}
+
+variable "web_redirect_uris" {
+  description = "(Optional) A set of URLs where OAuth 2.0 autorization codes and access tokens are sent."
+  type        = set(string)
+
+  validation {
+    condition     = alltrue([for uri in var.web_redirect_uris : can(regex("^https://|^ms-appx-web://|^http://localhost", uri))])
+    error_message = "All URIs must be valid HTTPS URLs excluding localhost."
+  }
+
+  default = []
+}
+
+variable "public_redirect_uris" {
+  description = "(Optional) A set of URLs where OAuth 2.0 autorization codes and access tokens are sent."
+  type        = set(string)
+
+  validation {
+    condition     = alltrue([for uri in var.public_redirect_uris : can(regex("^https://|^ms-appx-web://", uri))])
+    error_message = "All URIs must be valid HTTPS or ms-appx-web URLs."
+  }
+
+  default = []
+}
+
+variable "access_token_issuance" {
+  description = "(Optional) Should the application be allowed to request an access token?"
+  type        = bool
+  default     = false
+}
+
+variable "id_token_issuance" {
+  description = "(Optional) Should the application be allowed to request an ID token?"
+  type        = bool
+  default     = false
+}
+
+variable "required_resource_access" {
+  description = "(Optional) List of required resource access blocks"
+  type = list(object({
+    resource_app_id = string
+    resource_access = object({
+      id   = string
+      type = string
+    })
+  }))
+
+  validation {
+    condition     = alltrue([for resource in var.required_resource_access : resource.resource_access.type == "Role" || resource.resource_access.type == "Scope"])
+    error_message = "Type must be either \"Role\" or \"Scope\""
+  }
+
+  default = []
+}
+
+variable "oauth2_permission_scopes" {
+  description = "(Optional) List of required oauth permission scope"
+  type = list(object({
+    admin_consent_description  = string
+    admin_consent_display_name = string
+    enabled                    = optional(bool)
+    id                         = string
+    type                       = optional(string)
+    user_consent_description   = optional(string)
+    user_consent_display_name  = optional(string)
+    value                      = optional(string)
+  }))
+
+  validation {
+    condition     = alltrue([for scope in var.oauth2_permission_scopes : scope.type == "User" || scope.type == "Admin"])
+    error_message = "Type must be either \"User\" or \"Admin\""
+  }
+
+  default = []
 }
