3a9b319f0046a4216d202e1196938181a544f0b9
Sondre Sjølyst <sonyslyst@gmail.com>
2024-07-11 11:07:43 +0200

    feat: configure authentication (#3)
    Co-authored-by: Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+locals {
+  identifier_uris = {
+    # Set default identifier URI.
+    # Ref: https://learn.microsoft.com/en-us/entra/identity-platform/reference-app-manifest#identifieruris-attribute
+    "default" = "api://${azuread_application.this.client_id}"
+  }
+}
+
 data "azuread_client_config" "current" {}
 
 # Azure Active Directory provider version 2.44.0 adds a new simplified resource "azuread_application_registration" for
@@ -11,15 +19,83 @@ resource "azuread_application" "this" {
   owners                         = var.owners
   device_only_auth_enabled       = var.device_only_auth_enabled
   fallback_public_client_enabled = var.fallback_public_client_enabled
-  identifier_uris                = var.identifier_uris
   sign_in_audience               = "AzureADMyOrg"
 
+  public_client {
+    redirect_uris = var.public_client_redirect_uris
+  }
+
+  web {
+    homepage_url  = var.web_homepage_url
+    logout_url    = var.web_logout_url
+    redirect_uris = var.web_redirect_uris
+
+    implicit_grant {
+      access_token_issuance_enabled = var.access_token_issuance_enabled
+      id_token_issuance_enabled     = var.id_token_issuance_enabled
+    }
+  }
+
+  dynamic "required_resource_access" {
+    for_each = var.required_resource_accesses
+
+    content {
+      resource_app_id = required_resource_access.value.resource_app_id
+
+      dynamic "resource_access" {
+        for_each = required_resource_access.value.resource_accesses
+
+        content {
+          id   = resource_access.value.id
+          type = resource_access.value.type
+        }
+      }
+    }
+  }
+
+  api {
+    dynamic "oauth2_permission_scope" {
+      for_each = var.oauth2_permission_scopes
+
+      content {
+        admin_consent_description  = oauth2_permission_scope.value.admin_consent_description
+        admin_consent_display_name = oauth2_permission_scope.value.admin_consent_display_name
+
+        enabled = oauth2_permission_scope.value.enabled
+        id      = random_uuid.oauth2_permission_scope[oauth2_permission_scope.key].result
+        type    = oauth2_permission_scope.value.type
+
+        user_consent_description  = oauth2_permission_scope.value.user_consent_description
+        user_consent_display_name = oauth2_permission_scope.value.user_consent_display_name
+
+        value = oauth2_permission_scope.value.value
+      }
+    }
+  }
+
   lifecycle {
+    ignore_changes = [
+      # Configure identifier URIs using the standalone "azuread_application_identifier_uri" resource instead.
+      # This is required in order for the identifier URIs to reference the properties of the application, typically its application (client) ID.
+      identifier_uris
+    ]
+
     precondition {
       condition     = contains(var.owners, data.azuread_client_config.current.object_id)
       error_message = "Current client (object ID: \"${data.azuread_client_config.current.object_id}\") must be set as owner."
     }
   }
+}
+
+resource "random_uuid" "oauth2_permission_scope" {
+  count = length(var.oauth2_permission_scopes)
+}
+
+resource "azuread_application_identifier_uri" "this" {
+  for_each = merge(local.identifier_uris, var.identifier_uris)
+
+  application_id = azuread_application.this.id
+  identifier_uri = each.value
 }
 
 resource "azuread_service_principal" "this" {
--- variables.tf
+++ variables.tf
@@ -4,25 +4,25 @@ variable "display_name" {
 }
 
 variable "device_only_auth_enabled" {
-  description = "(Optional) Specifies whether this application supports device authentication without a user."
+  description = "Specifies whether this application supports device authentication without a user."
   type        = bool
   default     = false
 }
 
 variable "fallback_public_client_enabled" {
-  description = " (Optional) Specifies whether the application is a public client. Appropriate for apps using token grant flows that don't use a redirect URI."
+  description = "Specifies whether the application is a public client. Appropriate for apps using token grant flows that don't use a redirect URI."
   type        = bool
   default     = false
 }
 
 variable "identifier_uris" {
-  description = "(Optional) A set of user-defined URI(s) that uniquely identify an application within its Azure AD tenant, or within a verified custom domain if the application is multi-tenant."
-  type        = list(string)
-  default     = []
+  description = "A map of user-defined URIs that uniquely identify this application within its Microsoft Entra ID tenant, or within a verified custom domain if this application is multi-tenant."
+  type        = map(string)
+  default     = {}
 }
 
 variable "service_management_reference" {
-  description = "Reference application context information from a service management datbase, e.g. ServiceNow."
+  description = "Reference application context information from a service management database, e.g. ServiceNow."
   type        = string
 }
 
@@ -37,7 +37,97 @@ variable "owners" {
 }
 
 variable "login_url" {
-  description = "(Optional) The URL where the service provider redirects the user to Azure AD to authenticate. Azure AD uses the URL to launch the application from Microsoft 365 or the Azure AD My Apps. When blank, Azure AD performs IdP-initiated sign-on for applications configured with SAML-based single sign-on."
+  description = "The URL where the service provider redirects the user to Microsoft Entra ID to authenticate."
   type        = string
   default     = null
+}
+
+variable "web_homepage_url" {
+  description = "The home or landing page URL of this application."
+  type        = string
+  default     = null
+}
+
+variable "web_logout_url" {
+  description = "The URL that will be used by Microsoft's authorization service to sign out a user."
+  type        = string
+  default     = null
+}
+
+variable "web_redirect_uris" {
+  description = "A list of URLs where web OAuth 2.0 autorization codes and access tokens are sent."
+  type        = list(string)
+  default     = []
+
+  validation {
+    condition     = alltrue([for uri in var.web_redirect_uris : can(regex("^https://|^ms-appx-web://|^http://localhost", uri))])
+    error_message = "All URIs must be valid HTTPS URLs excluding localhost."
+  }
+}
+
+variable "public_client_redirect_uris" {
+  description = "A list of URLs where public client (e.g. mobile) OAuth 2.0 autorization codes and access tokens are sent."
+  type        = list(string)
+  default     = []
+
+  validation {
+    condition     = alltrue([for uri in var.public_client_redirect_uris : can(regex("^https://|^ms-appx-web://", uri))])
+    error_message = "All URIs must be valid HTTPS or ms-appx-web URLs."
+  }
+}
+
+variable "access_token_issuance_enabled" {
+  description = "Should this application be allowed to request an access token?"
+  type        = bool
+  default     = false
+}
+
+variable "id_token_issuance_enabled" {
+  description = "Should this application be allowed to request an ID token?"
+  type        = bool
+  default     = false
+}
+
+variable "required_resource_accesses" {
+  description = "A list of required resource accesses to configure for this application."
+  type = list(object({
+    resource_app_id = string
+    resource_accesses = list(object({
+      id   = string
+      type = string
+    }))
+  }))
+  default = []
+
+  validation {
+    condition     = alltrue([for resource in var.required_resource_accesses : alltrue([for access in resource.resource_accesses : access.type == "Role" || access.type == "Scope"])])
+    error_message = "Type must be either \"Role\" or \"Scope\"."
+  }
+}
+
+variable "oauth2_permission_scopes" {
+  description = "A list of required OAuth 2.0 permission scopes to configure for this application."
+  # Since the primary use of this variable is the creation of a dynamic nested
+  # block "azuread_application.this.api[0].oauth2_permission_scope", we'd
+  # usually set the type to 'list(object)'. However, this variable will also be
+  # used to create a repeatable resource 'random_uuid.oauth2_permission_scope'
+  # using the 'for_each' meta-argument, and with the 'for_each' meta-argument
+  # it's safer to use a map as an input. Using a list, changes to its contents
+  # could lead to a change of indexes, effectively changing Terraform resource
+  # identifiers and requiring those resources to be re-created.
+  type = map(object({
+    admin_consent_description  = string
+    admin_consent_display_name = string
+    enabled                    = bool
+    type                       = string
+    user_consent_description   = optional(string)
+    user_consent_display_name  = optional(string)
+    value                      = optional(string)
+  }))
+  default = {}
+
+  validation {
+    condition     = alltrue([for scope in var.oauth2_permission_scopes : scope.type == "User" || scope.type == "Admin"])
+    error_message = "Type must be either \"User\" or \"Admin\"."
+  }
 }
