cbdfcd45806e4d9f3ff3347ce290ace8f904e6c4
Henrik Simonsen Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-10-07 08:45:26 +0200

    feat: output Oauth2 permission scope IDs (#15)

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "application_id" {
+  description = "The ID of this application."
+  value       = azuread_application.this.id
+}
+
 output "application_object_id" {
   description = "The object ID of this application."
   value       = azuread_application.this.object_id
@@ -11,4 +16,11 @@ output "application_client_id" {
 output "service_principal_object_id" {
   description = "The object ID of this service principal."
   value       = azuread_service_principal.this.object_id
+}
+
+output "oauth2_permission_scope_ids" {
+  description = "A map of Oauth2 permission scope IDs."
+  value = {
+    for k, v in azuread_application.this.api[0].oauth2_permission_scope : k => v.id
+  }
 }
