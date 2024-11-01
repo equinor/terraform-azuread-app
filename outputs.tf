b03f639de79131ed91ac55b0be00540f23a43976
ErlendT <32453298+ErlendT@users.noreply.github.com>
2024-11-01 09:22:16 +0100

    feat: add app role ids output (#22)
    * feat: add app role ids output
    
    * docs: improve description
    
    Co-authored-by: Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,8 @@ output "api_oauth2_permission_scope_ids" {
   description = "A map of Oauth2 permission scope IDs."
   value       = azuread_application.this.oauth2_permission_scope_ids
 }
+
+output "app_role_ids" {
+  description = "A mapping of app role values to app role IDs, intended to be useful when referencing app roles in other resources in your configuration."
+  value       = azuread_application.this.app_role_ids
+}
